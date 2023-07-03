9a8f6bc02c584cdfd556e5b5732de04cfc98db4a
gouhar1512 <ushran987@gmail.com>
2023-07-03 14:43:28 +0530

    Subscribing the component to the store

--- src/Components/CakeContainer.js
+++ src/Components/CakeContainer.js
@@ -1,12 +1,30 @@
 import React from "react";
+import { buyCake } from "../redux";
+import { connect } from "react-redux";
 
-const CakeContainer = () => {
+const CakeContainer = (props) => {
   return (
     <div>
-      <h2>Number of cakes</h2>
-      <button>Buy Cake</button>
+      <h2>Number of cakes - {props.numOfCakes} </h2>
+      <button onClick={props.buyCake}>Buy Cake</button>
     </div>
   );
 };
 
-export default CakeContainer;
+// With this the state of our redux store is mapped to a prop in our component.
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
+
+// With this dispatch of an action creator is mapped to a prop in our component.
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+// `connect` connects state and dispatch to our component.
+// With this our component is now subscribed to the store.
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
