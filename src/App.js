d54e03ea8143cdad9ea54711c4cf3c90fc9f2e42
gouhar1512 <ushran987@gmail.com>
2023-07-03 15:20:05 +0530

    Using hooks to get state and dispatch actions

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Provider } from "react-redux";
 import "./App.css";
 import CakeContainer from "./Components/CakeContainer";
 import store from "./redux/store";
+import HooksCakeContainer from "./Components/HooksCakeContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeContainer />
         <CakeContainer />
       </div>
     </Provider>
